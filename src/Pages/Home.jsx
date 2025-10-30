ae0be2b342bf53b6144fb4ee396f5121c8d07eca
Janith-Nanayakkara <141998810+JanithNanayakkaraSE0612@users.noreply.github.com>
2025-10-30 11:56:12 +0530

    Update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,18 @@
-import Agents from "../Components/Agents"
-import Dream from "../Components/Dream"
-import Footer from "../Components/Footer"
-import { Hero } from "../Components/Hero"
-import OurProperties from "../Components/OurProperties"
-import Fab from "../Components/Fab"
-
-
+import Dream from "../Components/Dream";
+import Footer from "../Components/Footer";
+import { Hero } from "../Components/Hero";
+import OurProperties from "../Components/OurProperties";
+import Fab from "../Components/Fab";
 
 export const Home = () => {
   return (
     <div className="">
-        <Hero/>
-        <OurProperties/>
-        <Dream/>
-        <Agents/>  
-        <Footer/>
-        <Fab />
+      <Hero />
+      <OurProperties />
+      <Dream />
+      {/* <Agents/>   */}
+      <Footer />
+      <Fab />
     </div>
-  )
-}
+  );
+};
